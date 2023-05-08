61134aa1ff4a0ef7606fef12dd1e21628b2299b0
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-05-08 20:27:47 +0200

    Add prettier and code formatting pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import "reflect-metadata";
-import "./page.css"
+import "./page.css";
 import { StorageNode, StorageNodeSchema, StorageNodeView, TaskNode, TaskNodeSchema, TaskNodeView } from "./views";
 import { Container, ContainerModule } from "inversify";
-import {SEdge as SEdgeSchema, SGraph as SGraphSchema} from "sprotty-protocol";
-import { LocalModelSource, PolylineEdgeView, SEdge, SGraph, SGraphView, TYPES, boundsModule, configureModelElement, defaultModule, modelSourceModule, moveModule, routingModule, selectModule, viewportModule, zorderModule } from "sprotty";
+import { SEdge as SEdgeSchema, SGraph as SGraphSchema } from "sprotty-protocol";
+import {
+    LocalModelSource,
+    PolylineEdgeView,
+    SEdge,
+    SGraph,
+    SGraphView,
+    TYPES,
+    boundsModule,
+    configureModelElement,
+    defaultModule,
+    modelSourceModule,
+    moveModule,
+    routingModule,
+    selectModule,
+    viewportModule,
+    zorderModule,
+} from "sprotty";
 
 const taskModule = new ContainerModule((bind, unbind, isBound, rebind) => {
-    bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope()
+    bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope();
     const context = { bind, unbind, isBound, rebind };
-    configureModelElement(context, 'graph', SGraph, SGraphView);
-    configureModelElement(context, 'task', TaskNode, TaskNodeView);
-    configureModelElement(context, 'storage', StorageNode, StorageNodeView);
-    configureModelElement(context, 'edge', SEdge, PolylineEdgeView);
+    configureModelElement(context, "graph", SGraph, SGraphView);
+    configureModelElement(context, "task", TaskNode, TaskNodeView);
+    configureModelElement(context, "storage", StorageNode, StorageNodeView);
+    configureModelElement(context, "edge", SEdge, PolylineEdgeView);
 });
 
 const container = new Container();
@@ -25,41 +41,47 @@ const container = new Container();
 //     taskModule
 // );
 container.load(
-    defaultModule, modelSourceModule, boundsModule, viewportModule,
-    moveModule, routingModule, selectModule, zorderModule,
-    taskModule
+    defaultModule,
+    modelSourceModule,
+    boundsModule,
+    viewportModule,
+    moveModule,
+    routingModule,
+    selectModule,
+    zorderModule,
+    taskModule,
 );
 
 const graph: SGraphSchema = {
-    type: 'graph',
-    id: 'root',
+    type: "graph",
+    id: "root",
     children: [
         {
-            type: 'task',
-            id: 'task01',
-            name: 'First Task',
-            status: 'finished'
+            type: "task",
+            id: "task01",
+            name: "First Task",
+            status: "finished",
         } as TaskNodeSchema,
         {
-            type: 'task',
-            id: 'task02',
-            name: 'Second Task',
-            status: 'running'
+            type: "task",
+            id: "task02",
+            name: "Second Task",
+            status: "running",
         } as TaskNodeSchema,
         {
-            type: 'edge',
-            id: 'edge01',
-            sourceId: 'task01',
-            targetId: 'task02'
+            type: "edge",
+            id: "edge01",
+            sourceId: "task01",
+            targetId: "task02",
         } as SEdgeSchema,
         {
-            type: 'storage',
-            id: 'storage01',
-            name: 'TestDB',
-            position: { x: 100, y: 100 }
+            type: "storage",
+            id: "storage01",
+            name: "TestDB",
+            position: { x: 100, y: 100 },
         } as StorageNodeSchema,
-    ]
-}
+    ],
+};
 
-const modelSource = container.get<LocalModelSource>(TYPES.ModelSource)
-modelSource.setModel(graph)
+const modelSource = container.get<LocalModelSource>(TYPES.ModelSource);
+modelSource.setModel(graph);
--- src/views.tsx
+++ src/views.tsx
@@ -1,18 +1,18 @@
 /** @jsx svg */
-import {SNode as SNodeSchema} from "sprotty-protocol";
-import {svg, IView, SNode, RenderingContext} from "sprotty";
-import {injectable} from "inversify";
+import { SNode as SNodeSchema } from "sprotty-protocol";
+import { svg, IView, SNode, RenderingContext } from "sprotty";
+import { injectable } from "inversify";
 import { VNode } from "snabbdom";
 import "./views.css";
 
 export interface TaskNodeSchema extends SNodeSchema {
-    name?: string
-    status?: string
+    name?: string;
+    status?: string;
 }
 
 export class TaskNode extends SNode {
-    name: string = ''
-    status: string = ''
+    name: string = "";
+    status: string = "";
 }
 
 @injectable()
@@ -21,13 +21,22 @@ export class TaskNodeView implements IView {
         const radius = 20;
         // In this context, the coordinates (0,0) mark the upper left corner of
         // the node, thus we shift all elements by the radius of the circle.
-        return <g>
-            <circle class-sprotty-node={true} class-task={true}
-                    class-running={node.status === 'running'}
-                    class-finished={node.status === 'finished'}
-                    r={radius} cx={radius} cy={radius}></circle>
-            <text x={radius} y={radius + 5}>{node.name}</text>
-        </g>;
+        return (
+            <g>
+                <circle
+                    class-sprotty-node={true}
+                    class-task={true}
+                    class-running={node.status === "running"}
+                    class-finished={node.status === "finished"}
+                    r={radius}
+                    cx={radius}
+                    cy={radius}
+                ></circle>
+                <text x={radius} y={radius + 5}>
+                    {node.name}
+                </text>
+            </g>
+        );
     }
 }
 
@@ -36,7 +45,7 @@ export interface StorageNodeSchema extends SNodeSchema {
 }
 
 export class StorageNode extends SNode {
-    name: string = '';
+    name: string = "";
 }
 
 @injectable()
@@ -44,10 +53,14 @@ export class StorageNodeView implements IView {
     render(node: Readonly<StorageNode>, _context: RenderingContext): VNode {
         const width = 60;
         const height = 20;
-        return <g class-sprotty-node={true} class-storage={true}>
-            <line x1="0" y1="0" x2={width} y2="0"></line>
-            <text x={width / 2} y={height - 5}>{node.name}</text>
-            <line x1="0" y1={height} x2={width} y2={height}></line>
-        </g>;
+        return (
+            <g class-sprotty-node={true} class-storage={true}>
+                <line x1="0" y1="0" x2={width} y2="0"></line>
+                <text x={width / 2} y={height - 5}>
+                    {node.name}
+                </text>
+                <line x1="0" y1={height} x2={width} y2={height}></line>
+            </g>
+        );
     }
 }
