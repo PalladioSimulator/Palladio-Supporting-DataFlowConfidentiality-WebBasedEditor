baa6a5af985de9aeef0604f05f67b5f29ed36aba
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-05-10 20:15:39 +0200

    Add initial drag and drop support for function node only

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,29 @@ import {
     FunctionNodeSchema,
     FunctionNodeView,
     IONode,
+    IONodeSchema,
     IONodeView,
     StorageNode,
     StorageNodeSchema,
     StorageNodeView,
 } from "./views";
-import { Container, ContainerModule } from "inversify";
-import { SEdge as SEdgeSchema, SGraph as SGraphSchema } from "sprotty-protocol";
+import { Container, ContainerModule, inject, injectable } from "inversify";
+import {
+    Bounds,
+    SEdge as SEdgeSchema,
+    SGraph as SGraphSchema,
+    Action,
+    Point,
+    SNode as SNodeSchema,
+} from "sprotty-protocol";
 import {
     LocalModelSource,
+    MouseListener,
     PolylineEdgeView,
     SEdge,
     SGraph,
     SGraphView,
+    SModelElement,
     TYPES,
     boundsModule,
     configureModelElement,
@@ -29,12 +39,17 @@ import {
     moveModule,
     routingModule,
     selectModule,
+    updateModule,
     viewportModule,
     zorderModule,
 } from "sprotty";
 
-const taskModule = new ContainerModule((bind, unbind, isBound, rebind) => {
+// Setup the Dependency Injection Container.
+// This includes all used nodes, edges, listeners, etc. for sprotty.
+const dataFlowDiagramModule = new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope();
+    bind(DroppableMouseListener).toSelf().inSingletonScope();
+    bind(TYPES.MouseListener).toService(DroppableMouseListener);
     const context = { bind, unbind, isBound, rebind };
     configureModelElement(context, "graph", SGraph, SGraphView);
     configureModelElement(context, "storage", StorageNode, StorageNodeView);
@@ -43,6 +58,73 @@ const taskModule = new ContainerModule((bind, unbind, isBound, rebind) => {
     configureModelElement(context, "edge", SEdge, PolylineEdgeView);
 });
 
+@injectable()
+class DroppableMouseListener extends MouseListener {
+    @inject(TYPES.ModelSource) modelSource?: LocalModelSource;
+
+    override dragOver(_target: SModelElement, event: DragEvent): (Action | Promise<Action>)[] {
+        event.preventDefault();
+        return [];
+    }
+
+    override drop(_target: SModelElement, event: DragEvent): (Action | Promise<Action>)[] {
+        // console.log(_target, event)
+
+        const nodeType = event.dataTransfer?.getData("text/plain");
+        if (!nodeType) {
+            return [];
+        }
+
+        const nodeSize = 30;
+        modelSource.getViewport().then((viewport) => {
+            const newElement = this.addNode(nodeType, nodeSize, this.getVisibleBounds(viewport));
+            const adjust = (offset: number) => {
+                return offset / viewport.zoom - nodeSize / 2;
+            };
+            newElement.position = {
+                x: viewport.scroll.x + adjust(event.offsetX),
+                y: viewport.scroll.y + adjust(event.offsetY),
+            };
+            modelSource.addElements([newElement]);
+        });
+
+        return [];
+    }
+
+    private addNode(nodeType: string, nodeSize: number, bounds: Bounds): SNodeSchema {
+        return {
+            type: nodeType,
+            id: nodeType + Math.random().toString(36).substring(7),
+            name: "TestFunction",
+            position: {
+                x: bounds.x + Math.random() * (bounds.width - nodeSize),
+                y: bounds.y + Math.random() * (bounds.height - nodeSize),
+            },
+            size: {
+                width: nodeSize,
+                height: nodeSize,
+            },
+        } as FunctionNodeSchema;
+    }
+
+    private getVisibleBounds({
+        canvasBounds,
+        scroll,
+        zoom,
+    }: {
+        canvasBounds: Bounds;
+        scroll: Point;
+        zoom: number;
+    }): Bounds {
+        return {
+            ...scroll,
+            width: canvasBounds.width / zoom,
+            height: canvasBounds.height / zoom,
+        };
+    }
+}
+
+// Load the above defined module with all the used modules from sprotty.
 const container = new Container();
 // container.load(
 //     defaultModule, modelSourceModule, boundsModule, buttonModule,
@@ -51,7 +133,7 @@ const container = new Container();
 //     hoverModule, labelEditModule, labelEditUiModule, moveModule,
 //     openModule, routingModule, selectModule, undoRedoModule,
 //     updateModule, viewportModule, zorderModule, graphModule,
-//     taskModule
+//     dataFlowDiagramModule
 // );
 container.load(
     defaultModule,
@@ -61,10 +143,12 @@ container.load(
     moveModule,
     routingModule,
     selectModule,
+    updateModule,
     zorderModule,
-    taskModule,
+    dataFlowDiagramModule,
 );
 
+// Construct the diagram graph state that should be shown.
 const graph: SGraphSchema = {
     type: "graph",
     id: "root",
@@ -86,7 +170,7 @@ const graph: SGraphSchema = {
             id: "input01",
             name: "TestInput",
             position: { x: 300, y: 300 },
-        },
+        } as IONodeSchema,
 
         {
             type: "edge",
@@ -103,5 +187,10 @@ const graph: SGraphSchema = {
     ],
 };
 
+// Load the graph into the model source and display it inside the DOM.
+// Unless overwritten this will load the graph into the DOM element with the id "sprotty".
 const modelSource = container.get<LocalModelSource>(TYPES.ModelSource);
-modelSource.setModel(graph);
+modelSource
+    .setModel(graph)
+    .then(() => console.log("Sprotty model set."))
+    .catch((reason) => console.error(reason));
