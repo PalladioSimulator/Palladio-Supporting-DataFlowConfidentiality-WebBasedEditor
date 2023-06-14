b1a1e0d370c7940e711baff87997f2af9535911f
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-06-14 22:16:46 +0200

    Refactor load diagram command

--- src/commands/load.ts
+++ src/commands/load.ts
@@ -1,6 +1,15 @@
 import { inject } from "inversify";
-import { Command, CommandExecutionContext, ILogger, LocalModelSource, NullLogger, SModelRoot, TYPES } from "sprotty";
-import { Action } from "sprotty-protocol";
+import {
+    Command,
+    CommandExecutionContext,
+    EMPTY_ROOT,
+    ILogger,
+    NullLogger,
+    SModelElementSchema,
+    SModelRoot,
+    TYPES,
+} from "sprotty";
+import { Action, SModelRoot as SModelRootSchema } from "sprotty-protocol";
 
 export interface LoadDiagramAction extends Action {
     kind: typeof LoadDiagramAction.KIND;
@@ -17,16 +26,12 @@ export namespace LoadDiagramAction {
 
 export class LoadDiagramCommand extends Command {
     static readonly KIND = LoadDiagramAction.KIND;
-    @inject(TYPES.ModelSource)
-    private modelSource: LocalModelSource = new LocalModelSource();
     @inject(TYPES.ILogger)
     private logger: ILogger = new NullLogger();
+    private oldRoot: SModelRoot | undefined;
+    private newRoot: SModelRoot | undefined;
 
-    constructor() {
-        super();
-    }
-
-    execute(context: CommandExecutionContext): SModelRoot {
+    async execute(context: CommandExecutionContext): Promise<SModelRoot> {
         // Open a file picker dialog.
         // The cleaner way to do this would be showOpenFilePicker(),
         // but safari and firefox don't support it at the time of writing this code:
@@ -34,7 +39,7 @@ export class LoadDiagramCommand extends Command {
         const input = document.createElement("input");
         input.type = "file";
         input.accept = ".json";
-        const fileLoadPromise = new Promise((resolve, reject) => {
+        const fileLoadPromise = new Promise<SModelRootSchema>((resolve, reject) => {
             input.onchange = () => {
                 if (input.files && input.files.length > 0) {
                     const file = input.files[0];
@@ -55,35 +60,49 @@ export class LoadDiagramCommand extends Command {
         });
         input.click();
 
-        fileLoadPromise
-            .then((model: any) => {
-                // TODO: document, move to dedicated class method
-                const removeFeatures = (model: any) => {
-                    delete model.features;
-                    if (model.children) {
-                        model.children.forEach((child: any) => removeFeatures(child));
-                    }
-                };
-                removeFeatures(model);
+        this.oldRoot = context.root;
+        try {
+            const newSchema = await fileLoadPromise;
+            this.preprocessModelSchema(newSchema);
+            this.newRoot = context.modelFactory.createRoot(newSchema);
 
-                return this.modelSource.setModel(model);
-            })
-            .then(() => {
-                this.logger.info(this, "Model loaded successfully");
-            })
-            .catch((error) => {
-                this.logger.error(this, "Error loading model", error);
-            });
-
-        return context.root;
+            this.logger.info(this, "Model loaded successfully");
+            return this.newRoot;
+        } catch (error) {
+            this.logger.error(this, "Error loading model", error);
+            return this.oldRoot;
+        }
     }
 
-    // TODO: undo and redo implementation
+    /**
+     * Before a saved model schema can be loaded, it needs to be preprocessed.
+     * Currently this means that the features property is removed from all model elements recursively,
+     * but in the future more thing may be added here.
+     *
+     * The feature property at runtime is a js Set with the relevant features.
+     * E.g. for the top graph this is the viewportFeature among others.
+     * When converting js Sets objects into json, the result is an empty js object.
+     * When loading the object is converted into an empty js Set and the features are lost.
+     * Because of this the editor won't work properly after loading a model.
+     * To prevent this, the features property is removed before loading the model.
+     * When the features property is missing it gets rebuilt on loading with the currently used features.
+     *
+     * @param modelSchema The model schema to preprocess
+     */
+    private preprocessModelSchema(modelSchema: SModelRootSchema): void {
+        // Feature is not included in the typing
+        "features" in modelSchema && delete modelSchema["features"];
+
+        if (modelSchema.children) {
+            modelSchema.children.forEach((child: any) => this.preprocessModelSchema(child));
+        }
+    }
+
     undo(context: CommandExecutionContext): SModelRoot {
-        return context.root;
+        return this.oldRoot ?? context.modelFactory.createRoot(EMPTY_ROOT);
     }
 
     redo(context: CommandExecutionContext): SModelRoot {
-        return context.root;
+        return this.newRoot ?? context.modelFactory.createRoot(EMPTY_ROOT);
     }
 }
