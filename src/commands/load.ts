2691cce6927ac5a63c9e6644eb767b77b9e38b3b
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-06-18 22:49:17 +0200

    Don't hang editor when cancelling the file load dialog

--- src/commands/load.ts
+++ src/commands/load.ts
@@ -30,7 +30,8 @@ export class LoadDiagramCommand extends Command {
         const input = document.createElement("input");
         input.type = "file";
         input.accept = ".json";
-        const fileLoadPromise = new Promise<SModelRootSchema>((resolve, reject) => {
+        const fileLoadPromise = new Promise<SModelRootSchema | undefined>((resolve, reject) => {
+            // This event is fired when the user successfully submits the file picker dialog.
             input.onchange = () => {
                 if (input.files && input.files.length > 0) {
                     const file = input.files[0];
@@ -48,12 +49,35 @@ export class LoadDiagramCommand extends Command {
                     reject("No file selected");
                 }
             };
+            // The focus event is fired when the file picker dialog is closed.
+            // This includes cases where a file was selected and when the dialog was canceled and no file was selected.
+            // If a file was selected the change event above is fired after the focus event.
+            // So if a file was selected the focus event should be ignored and the promise is resolved in the onchange handler.
+            // If the file dialog was canceled undefined should be resolved by the focus handler.
+            // Because we don't know whether the change event will follow the focus event,
+            // we have a 300ms timeout before resolving the promise.
+            // If the promise was already resolved by the onchange handler, this won't do anything.
+            window.addEventListener(
+                "focus",
+                () => {
+                    setTimeout(() => {
+                        resolve(undefined);
+                    }, 300);
+                },
+                { once: true },
+            );
         });
         input.click();
 
         this.oldRoot = context.root;
         try {
             const newSchema = await fileLoadPromise;
+            if (!newSchema) {
+                this.logger.info(this, "Model loading aborted");
+                this.newRoot = this.oldRoot;
+                return this.oldRoot;
+            }
+
             this.preprocessModelSchema(newSchema);
             this.newRoot = context.modelFactory.createRoot(newSchema);
 
@@ -61,6 +85,7 @@ export class LoadDiagramCommand extends Command {
             return this.newRoot;
         } catch (error) {
             this.logger.error(this, "Error loading model", error);
+            this.newRoot = this.oldRoot;
             return this.oldRoot;
         }
     }
